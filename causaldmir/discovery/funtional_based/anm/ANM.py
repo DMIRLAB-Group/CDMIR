af44342c37b309e057a3e647495089a47814eb75
kanseaveg <taroballscai@hotmail.com>
2022-11-09 14:43:05 +0800

    add the Additive Noise Model to causaldmir.discovery.function_based.

---
+++ causaldmir/discovery/funtional_based/anm/ANM.py
@@ -0,0 +1,106 @@
+"""
+Additive Noise Model.
+"""
+
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.preprocessing import scale
+import numpy as np
+
+
+def rbf_dot2(p1, p2, deg):
+    if p1.ndim == 1:
+        p1 = p1[:, np.newaxis]
+        p2 = p2[:, np.newaxis]
+
+    size1 = p1.shape
+    size2 = p2.shape
+
+    G = np.sum(p1 * p1, axis=1)[:, np.newaxis]
+    H = np.sum(p2 * p2, axis=1)[:, np.newaxis]
+    Q = np.tile(G, (1, size2[0]))
+    R = np.tile(H.T, (size1[0], 1))
+    H = Q + R - 2.0 * np.dot(p1, p2.T)
+    H = np.exp(-H / 2.0 / (deg ** 2))
+
+    return H
+
+
+def rbf_dot(X, deg):
+    # Set kernel size to median distance between points, if no kernel specified
+    if X.ndim == 1:
+        X = X[:, np.newaxis]
+    m = X.shape[0]
+    G = np.sum(X * X, axis=1)[:, np.newaxis]
+    Q = np.tile(G, (1, m))
+    H = Q + Q.T - 2.0 * np.dot(X, X.T)
+    if deg == -1:
+        dists = (H - np.tril(H)).flatten()
+        deg = np.sqrt(0.5 * np.median(dists[dists > 0]))
+    H = np.exp(-H / 2.0 / (deg ** 2))
+
+    return H
+
+
+def FastHsicTestGamma(X, Y, sig=[-1, -1], maxpnt=200):
+
+    m = X.shape[0]
+    if m > maxpnt:
+        indx = np.floor(np.r_[0:m:float(m - 1) / (maxpnt - 1)]).astype(int)
+        #       indx = np.r_[0:maxpnt]
+        Xm = X[indx].astype(float)
+        Ym = Y[indx].astype(float)
+        m = Xm.shape[0]
+    else:
+        Xm = X.astype(float)
+        Ym = Y.astype(float)
+
+    H = np.eye(m) - 1.0 / m * np.ones((m, m))
+
+    K = rbf_dot(Xm, sig[0])
+    L = rbf_dot(Ym, sig[1])
+
+    Kc = np.dot(H, np.dot(K, H))
+    Lc = np.dot(H, np.dot(L, H))
+
+    testStat = (1.0 / m) * (Kc.T * Lc).sum()
+    if ~np.isfinite(testStat):
+        testStat = 0
+
+    return testStat
+
+
+def normalized_hsic(x, y):
+    x = (x - np.mean(x)) / np.std(x)
+    y = (y - np.mean(y)) / np.std(y)
+    h = FastHsicTestGamma(x, y)
+
+    return h
+
+
+class ANM(object):
+
+    def __init__(self):
+        """Init the model."""
+        super(ANM, self).__init__()
+
+    def cause_or_effect(self, x, y, **kwargs):
+        """
+        Parameters: data_x, data_y
+        Returns: p_value_forward: p value in the x->y direction
+                 p_value_backward: p value in the y->x direction
+        """
+        x = scale(x).reshape((-1, 1))
+        y = scale(y).reshape((-1, 1))
+        # calculate the x->y score
+        p_value_forward = self.anm_score(x, y)
+        # calculate the y->x score
+        p_value_backward = self.anm_score(y, x)
+
+        return p_value_forward, p_value_backward
+
+    def anm_score(self, x, y):
+        gp = GaussianProcessRegressor().fit(x, y)
+        y_predict = gp.predict(x)
+        indepscore = normalized_hsic(y_predict - y, x)
+
+        return indepscore
