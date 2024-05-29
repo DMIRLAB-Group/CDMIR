1d07859adb45d4133a5ce0c66d1e5a3221ba42d7
Pansyyyy <1964249854@qq.com>
2024-05-29 15:39:21 +0800

    correct the asserterror

--- causaldmir/utils/kernel/test/test_gaussian.py
+++ causaldmir/utils/kernel/test/test_gaussian.py
@@ -10,24 +10,20 @@ def test_gaussian_case1(): #shape(x)[1]==shape(y)[1]
     ys=np.random.randn(30,20)
 
     gk=GaussianKernel()
-    print(gk(xs,ys))
+    gk(xs,ys)
+    print(gk.__call__(xs,ys))
+    # print(gk(xs,ys))
 
-def test_gaussian_case2():#shape(x)[1]!=shape(y)[1]
-    np.random.seed(10)
-    xs = np.random.randn(20, 20)
-    ys = np.random.randn(30, 30)
 
-    gk = GaussianKernel()
-    print(gk(xs, ys))
-
-def test_gaussian_case3():#y is none
+def test_gaussian_case2():#y is none
     np.random.seed(10)
     xs = np.random.randn(20, 20)
     #ys = np.array([])
 
     gk = GaussianKernel()
-    print(gk(xs, ys=None))
+    gk(xs,ys=None)
+    print(gk.__call__(xs,ys=None))
+    # print(gk(xs, ys=None))
 
-#test_gaussian_case1()
-#test_gaussian_case2()
-#test_gaussian_case3()
+test_gaussian_case1()
+# test_gaussian_case2()
